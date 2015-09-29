774ecea3369859d9d142f06e21eb106296b14134
root <root@apache.opencontent.it>
2015-09-29 13:48:41 +0200

    aggiunto anche scan di directory ezpublish_legacy

--- ezscan_server.sh
+++ ezscan_server.sh
@@ -10,7 +10,7 @@ EZSCAN=`which ezscan`
 HOSTNAME_COMMAND=`which hostname`
 THIS_SCRIPT=`basename $0`
 
-ez_dirs=`echo /home/httpd/*/html | xargs $EZSCAN -qD`
+#ez_dirs=`echo /home/httpd/*/html | xargs $EZSCAN -qD`
 
 #ez_dirs="/home/httpd/demo.opencontent.it/html"
 
@@ -19,26 +19,34 @@ echo " <server>"
 echo " <name>`$HOSTNAME_COMMAND -f`</name>"
 echo " <date>`date`</date>"
 echo " <ez_dirs>"
-for dir in $ez_dirs ; do
-  echo "  <ez_dir>"
-  echo "   <document_root>`$EZSCAN -D $dir`</document_root>"
-  echo "   <ez_version>`$EZSCAN -z $dir`</ez_version>"
-  echo "   <virtualhost>`$EZSCAN -w $dir`</virtualhost>"
-  siteaccess_list=`$EZSCAN -S $dir`
-  echo "   <siteaccesses>"
-  for siteaccess in $siteaccess_list ; do
-    echo "    <siteaccess>"
-    echo "     <name>$siteaccess</name>"
-    echo "     <DatabaseSettings>"
-    echo "      <DatabaseImplementation>`$EZSCAN  -s $siteaccess -B DatabaseSettings -V Database $dir`</DatabaseImplementation>"
-    echo "      <Server>`$EZSCAN  -s $siteaccess -B DatabaseSettings -V Server $dir`</Server>"
-    echo "      <User>`$EZSCAN  -s $siteaccess -B DatabaseSettings -V Database $dir`</User>"
-    echo "      <Database>`$EZSCAN  -s $siteaccess -B DatabaseSettings -V Database $dir`</Database>"
-    echo "     </DatabaseSettings>"
-    echo "    </siteaccess>"
+
+
+for dir_modello in '/home/httpd/*/html' '/home/httpd/*/html/ezpublish_legacy' ; do
+  #ez_dirs=`echo /home/httpd/*/html | xargs $EZSCAN -qD`
+  ez_dirs=`echo $dir_modello | xargs $EZSCAN -qD`
+
+  for dir in $ez_dirs ; do
+    echo "  <ez_dir>"
+    echo "   <document_root>`$EZSCAN -D $dir`</document_root>"
+    echo "   <ez_version>`$EZSCAN -z $dir`</ez_version>"
+    echo "   <virtualhost>`$EZSCAN -w $dir`</virtualhost>"
+    siteaccess_list=`$EZSCAN -S $dir`
+    echo "   <siteaccesses>"
+    for siteaccess in $siteaccess_list ; do
+      echo "    <siteaccess>"
+      echo "     <name>$siteaccess</name>"
+      echo "     <DatabaseSettings>"
+      echo "      <DatabaseImplementation>`$EZSCAN  -s $siteaccess -B DatabaseSettings -V Database $dir`</DatabaseImplementation>"
+      echo "      <Server>`$EZSCAN  -s $siteaccess -B DatabaseSettings -V Server $dir`</Server>"
+      echo "      <User>`$EZSCAN  -s $siteaccess -B DatabaseSettings -V Database $dir`</User>"
+      echo "      <Database>`$EZSCAN  -s $siteaccess -B DatabaseSettings -V Database $dir`</Database>"
+      echo "     </DatabaseSettings>"
+      echo "    </siteaccess>"
+    done
+    echo "   </siteaccesses>"
+    echo "  </ez_dir>"
   done
-  echo "   </siteaccesses>"
-  echo "  </ez_dir>"
 done
 echo " </ez_dirs>"
 echo " </server>"
+
